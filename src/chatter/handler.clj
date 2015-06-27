74d660133c81750cd98bc91993b37c202827197f
Melissa Aune <melissaaune12@gmail.com>
2015-06-27 14:09:46 -0500

    update to code

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -3,16 +3,6 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [hiccup.page :as page]))
-
-(defroutes app-routes
-  (GET "/" []
-       (page/html5
-        [:head
-         [:title "chatter"]]
-        [:body
-         [:h1 "Our Chat App"]]))
-  (route/not-found "Not Found"))
-
 (def app
   (wrap-defaults app-routes site-defaults))
 (defn generate-message-view
